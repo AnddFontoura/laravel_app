f85c3ea73d7acaa31487902d89c2f11652388823
André Fontoura <45948615+AnddFontoura@users.noreply.github.com>
2023-04-18 20:15:53 -0300

    Initial commit

---
+++ database/migrations/2023_04_18_221854_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 254)->nullable(false);
+            $table->text('description', 10000)->nullable(true);
+            $table->float('price', 10, 2)->nullable(false);
+            $table->string('main_image', 1000)->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
